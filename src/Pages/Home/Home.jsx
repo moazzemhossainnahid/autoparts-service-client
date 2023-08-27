a47391b91456d7543450257949ffa63f27bfb5e6
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-08-27 22:06:59 +0600

    resolved

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import HomeBanner from '../../Components/Pages/Home/HomeBanner';
 import Newsletter from '../../Components/Pages/Home/Newsletter';
 import AboutUs from '../../Components/Pages/Home/AboutUs';
+import PartsThress from '../../Components/Pages/Home/PartsThress';
+import ServiceThree from '../../Components/Pages/Home/ServiceThree';
 
 const Home = () => {
     return (
         <div className='w-full'>
             <HomeBanner/>
+            <PartsThress/>
+            <ServiceThree/>
             <AboutUs/>
             <Newsletter/>
         </div>
